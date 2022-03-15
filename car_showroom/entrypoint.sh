352e923bbe54f14e1acd0f9ab12075cf258b665c
easylager <oceandrinkparadise@gmail.com>
2022-03-15 17:39:29 +0300

    corrected entrypoint conection

--- car_showroom/entrypoint.sh
+++ car_showroom/entrypoint.sh
@@ -11,9 +11,8 @@ then
     echo "PostgreSQL started"
 fi
 
-#python manage.py flush --no-input
-#python manage.py makemigrations
-#python manage.py migrate
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
 
 
 exec "$@"
